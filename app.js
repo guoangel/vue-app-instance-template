a23b24831221306f2bf3681bb17d6ac22207b425
angel <fsguoangel@hotmail.com>
2022-05-02 20:11:58 +0800

    Computed Properties

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const app = Vue.createApp({
+let taxRate = 1.23;
+const vm = Vue.createApp({
   data() {
     return {
-      counter: 0
+      price: 4,
+      items: 0
     }
   },
-  methods: {
-    increment() {
-      this.counter++;
+  computed: {
+    taxRate() {
+      return (taxRate*100)-100;
+    },
+    includingVat() {
+      return (this.price*taxRate).toFixed(2);
+    },
+    total() {
+      return (this.includingVat*this.items).toFixed(2);
     }
   }
-});
-const vm = app.mount('#app');
+}).mount('#app');
