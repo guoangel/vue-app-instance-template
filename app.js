4c8625208d0f93cd030b657e1dad2c3329878e9e
angel <fsguoangel@hotmail.com>
2022-05-05 20:07:29 +0800

    passing data to components

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = Vue.createApp({
   computed: {}
 });
 app.component('hello-world-item', {
-  template: `<div>Hello World!</div>`,
+  props:['task'],
+  template: `<div class="bg-white shadow-sm rounded-md text-gray-700 text-xs md:text-sm p-4">{{task}}</div>`,
 });
 app.mount('#app');
