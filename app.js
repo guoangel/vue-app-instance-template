fd4a5fb96cd5680686843f2913c669f6f1a24b1f
angel <fsguoangel@hotmail.com>
2022-05-05 21:10:45 +0800

    v-for with components

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
+let nextTaskId = 100;
+
 const app = Vue.createApp({
   data() {
-    return {}
-  },
-  methods: {},
-  computed: {}
-});
-app.component('hello-world-item', {
-  props:{
-    task: {
-      type: String,
-      required: true,
-      validator(value) {
-        return "Okay" === value;
-      }
-    },
-    id: {
-      required: true,
-      type: Number,
-      validator(value) {
-        return value >=1 && value <=100;
-      },
-      default() {
-        return 50;
-      }
+    return {
+      tasks: []
     }
   },
-  template: `<div class="bg-white shadow-sm rounded-md text-gray-700 text-xs md:text-sm p-4">{{task}}</div>`,
+  methods: {
+    taskAdded(task) {
+      this.tasks.push({
+        id: nextTaskId++,
+        description: task,
+        done: false,
+        priority: false
+      });
+    }
+  },
+  computed: {}
 });
+app.component('todo-list-item', {
+  props:{
+    task: {
+      type: Object
+    }
+  },
+  template: `<div class="bg-white shadow-sm rounded-md text-gray-700 text-xs md:text-sm p-4">{{task.description}}</div>`,
+});
+
+app.component('add-task-input', {
+  emits: ['added'],
+  data() {
+    return {
+      task: ''
+    }
+  },
+  methods: {
+    add() {
+      this.$emit('added', this.task);
+      this.task='';
+    }
+  },
+  template: `<input type="text" placeholder="Enter task and hit enter" @keyup.enter="add" v-model="task" class="block w-full rounded-md shadow-sm text-lg p-4" />`,
+});
+
 app.mount('#app');
