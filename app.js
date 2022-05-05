ff37c4deb3dfa60867c9ee1e777ddf467898105d
angel <fsguoangel@hotmail.com>
2022-05-05 20:18:16 +0800

    prop attributes

--- app.js
+++ app.js
@@ -6,7 +6,25 @@ const app = Vue.createApp({
   computed: {}
 });
 app.component('hello-world-item', {
-  props:['task'],
+  props:{
+    task: {
+      type: String,
+      required: true,
+      validator(value) {
+        return "Okay" === value;
+      }
+    },
+    id: {
+      required: true,
+      type: Number,
+      validator(value) {
+        return value >=1 && value <=100;
+      },
+      default() {
+        return 50;
+      }
+    }
+  },
   template: `<div class="bg-white shadow-sm rounded-md text-gray-700 text-xs md:text-sm p-4">{{task}}</div>`,
 });
 app.mount('#app');
